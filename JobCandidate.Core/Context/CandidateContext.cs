629f61fbe014fcd9bdcde05df7d98cfa871e4c9f
Smaran Luitel <smaran.luitel1@gmail.com>
2024-12-22 09:57:28 +0545

    Add and initialize database entities using central core library

---
+++ JobCandidate.Core/Context/CandidateContext.cs
@@ -0,0 +1,9 @@
+﻿using JobCandidate.Core.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace JobCandidate.Core.Context;
+
+public class CandidateContext(DbContextOptions<CandidateContext> options) : DbContext(options)
+{
+    public DbSet<Candidate> Candidates { get; set; }
+}
---
+++ JobCandidate.Core/Entity/Candidate.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JobCandidate.Core.Entity;
+
+public class Candidate
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    public string FirstName { get; set; }
+
+    [Required]
+    public string LastName { get; set; }
+
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; }
+
+    [Phone]
+    public string PhoneNumber { get; set; }
+
+    public string CallTimeInterval { get; set; }
+
+    [Url]
+    public string LinkedIn { get; set; }
+
+    [Url]
+    public string GitHub { get; set; }
+
+    [Required]
+    public string Comment { get; set; }
+}
